043ab0933834562f07e0d897ff1fad2f31291c3d
Jorge Gutiérrez Cordero <jorge.guti.estudiantil@gmail.com>
2020-11-14 00:45:41 -0600

    App usage read queries done as well of backend capaility to perform read operations. Pending making the routes that use them together

--- API/controllers/Vigilantee.ts
+++ API/controllers/Vigilantee.ts
@@ -11,14 +11,75 @@ export class Vigilantee {
         let log = {
             GUID      : pGUID,
             Location  : pLocation,
-            Canton: pCanton,
-            Province : pProvice,
+            Canton    : pCanton,
+            Province  : pProvice,
             TimeStamp : timeStamp,
             Status    : pStatus,
             Feedback  : pFeedback
         }
-
         mongoDriver.write("AlertMe", "Logs", log)
+    }
+
+    public static async getDailyActivity(weekday : number){
+        
+        let query =      
+        [
+            {
+                $project:
+                    {
+                    hour: { $hour: "$TimeStamp" },
+                    weekDay: { $dayOfWeek: "$TimeStamp" }
+                    }
+            },
+            {
+                $match:
+                {
+                    weekDay : 1
+                }
+            },
+            {
+                $group:
+                {
+                    _id: { hour : "$hour" },
+                    count:{ $sum : 1 }
+                }
+            },
+            {
+                $sort : 
+                { 
+                    "_id.time" : 1
+                }
+            }
+        
+        ]
+    }
+    
+    public static async getWeeklyActivity(){
+        let query =
+        [
+            {
+            $project:
+                {
+                dayOfWeek: { $dayOfWeek: "$TimeStamp" }
+                }
+            },
+            {
+                $group: 
+                { 
+                _id:   {weekDay:"$dayOfWeek"}, 
+                count: { $sum: 1 }
+                } 
+            },     
+            {
+                $sort : 
+                { 
+                "_id.weekDay" : 1
+                }
+            }
+        ]
+    }
+    
+    public static async getIntersections(){
 
     }
 
--- API/routes/alertme.ts
+++ API/routes/alertme.ts
@@ -1,14 +1,41 @@
 import * as express from 'express' 
 import e = require('express')
 import { Logger } from '../common' 
-import {Vigilantee} from '../controllers'
+import {MongoDriver, Vigilantee} from '../controllers'
 const app = express() 
 const router = express.Router();
 
 //Request principal
 app.post('/log/:guid/:location/:Canton/:Provice/:status/:feedback?', async (req, res) => {
     Vigilantee.alertMe(req.params.guid, req.params.location,req.params.Canton,req.params.Province,req.params.status,req.params.feedback)
-    res.send("hola mundo")
+    .then( 
+      () => {
+        res.send("hola mundo")
+      }
+    )
+  }
+)
+
+
+// Test query - fully functioning
+app.get('/info/test', async (req, res) => {
+  
+    MongoDriver.getInstance().find('AlertMe', 'Logs', { Canton : "Naranjo"})
+    .then
+    (
+      (result : any)=>
+      {
+        res.send(result)
+      }
+    )
+    .catch
+    (
+      (error) =>
+      {
+        Logger.error("en route")
+        Logger.error(error)
+      }
+    )
   }
 )
 
