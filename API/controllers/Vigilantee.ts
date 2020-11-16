9f24b365b5cef6e4b838aa311ddb64c77ba0c7f6
Carlos <carlosjvare@gmail.com>
2020-11-16 17:26:16 -0600

    Added geopositional query

--- API/controllers/Vigilantee.ts
+++ API/controllers/Vigilantee.ts
@@ -120,7 +120,51 @@ export class Vigilantee {
     }
     
     public static async getIntersections(){
+        let mongoDriver = MongoDriver.getInstance();
+        let commonIntersections = new Promise(
+            (resolve, reject) => 
+            {
+                let intersections: unknown = [];
+                try
+                {
+                mongoDriver.find("AlertMe","Logs","").then(logs=>
+                    {
+                        let keys = Object.keys(logs);
+                        keys.forEach(key => {
+                            let currentCoordinates= logs[key].Location.coordinates
+                            let getIntersect =
+                            [
+                                {
+                                    $geoNear:
+                                    {
+                                        near:{coordinates:currentCoordinates},
+                                        distanceField: "calcDistance",
+                                        maxDistance:5
+                                    }  
+                                },
+                                {
+                                    $group : {_id:null,count:{$sum:1}}
+                                },
+                                {
+                                    "$match": {"count":{"$gte":5} } 
+                                } 
+                            ]
+                              MongoDriver.getInstance().aggregate("AlertMe","Logs",getIntersect).then(inter=>
+                                {
+                                  intersections.push([currentCoordinates,inter.count])
+                                })
+                            });
+                        resolve(intersections)
+                    }
+                )
+                }
+                catch(error)
+                {
+                    reject(error)
+                }
 
+            }
+        )
     }
 
 }
