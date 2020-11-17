806fc4cc2806609466a908eb9f5e438d2bdd198b
Jorge Gutiérrez Cordero <jorge.guti.estudiantil@gmail.com>
2020-11-17 12:29:29 -0600

    CORS problem solved version 2 - fetch() with POST option

--- API/routes/alertme.ts
+++ API/routes/alertme.ts
@@ -6,7 +6,7 @@ const app = express()
 const router = express.Router();
 
 //Request principal
-app.get('/log/:guid/:lat/:lng/:Canton/:Provice/:status/:feedback?', async (req, res) => {
+app.post('/log/:guid/:lat/:lng/:Canton/:Provice/:status/:feedback?', async (req, res) => {
   Vigilantee.alertMe(req.params.guid,+req.params.lat,+req.params.lng,req.params.Canton,req.params.Province,req.params.status,req.params.feedback).then(()=>{
       res.json({ok:1})
     })
