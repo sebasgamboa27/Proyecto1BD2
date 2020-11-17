7bb25d3469c2898133cd1b0758d44c60f6bca676
Carlos <carlosjvare@gmail.com>
2020-11-17 14:39:10 -0600

    PowerBi Dataset Push Integration Complete

--- API/common/constants/constants.ts
+++ API/common/constants/constants.ts
@@ -14,6 +14,8 @@ export class Constants {
 
     public static CONNECTION_ERROR_MSG = "Unable to connect to mongo."
 
+    public static POWERBI_DATAPUSH_ERROR_MSG = "Couldn't connect to the PowerBi service"
+
     public static CONNECTED_MSG = "Mongo connection stablished."
 
     public static CONNECTION_CLOSED = "Mongo connection closed."
@@ -21,5 +23,9 @@ export class Constants {
     public static POOL_SIZE = 20
     
     public static GEOPOSITIONAL_INTERSECTION_MINIMUM = 2
+
+    public static GEOPOSITIONAL_INDEX_RESULT = 0
+
+    public static POWERBI_HOST = "https://api.powerbi.com/beta/bfcf1d9d-93ea-43b1-b902-1daa68a64248/datasets/7e4e7bef-39fd-4825-acd8-4b0680079575/rows?key=qB%2BOaNFxazSGJOB%2BFrXLbKRUeV%2BpmKfY9xFooXHlhr9X5q7lcRqndhSyt42%2FeoDQdoQh%2Fcp12xJQ75zRzO2Y7w%3D%3D"
 
 }
