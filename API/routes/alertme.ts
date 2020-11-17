3d5a8e6844baa04e08aff65b07c3226715d3909b
Carlos <carlosjvare@gmail.com>
2020-11-16 18:48:36 -0600

    Connection issues fixed

--- API/routes/alertme.ts
+++ API/routes/alertme.ts
@@ -7,7 +7,9 @@ const router = express.Router();
 
 //Request principal
 app.post('/log/:guid/:location/:Canton/:Provice/:status/:feedback?', async (req, res) => {
-    Vigilantee.alertMe(req.params.guid, req.params.location,req.params.Canton,req.params.Province,req.params.status,req.params.feedback)
+    Vigilantee.alertMe(req.params.guid, req.params.location,req.params.Canton,req.params.Province,req.params.status,req.params.feedback).then(()=>{
+      res.json({ok:1})
+    })
   }
 )
 
